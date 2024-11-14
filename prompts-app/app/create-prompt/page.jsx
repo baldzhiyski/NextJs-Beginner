fb666787f425c1a08c2495cfaca813b8b8694131
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-11-14 18:03:31 +0100

    Implementing endpoint for creating the prompt

--- prompts-app/app/create-prompt/page.jsx
+++ prompts-app/app/create-prompt/page.jsx
@@ -1,31 +1,51 @@
-'use client'
-import {use, useState} from 'react';
-import {useSession} from 'next-auth/react';
-import {useRouter} from 'next/navigation';
-import Form from '@components/Form';
-import { set } from 'mongoose';
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-    const [submitting , setSubmitting ] = useState(false);
-    const [post, setPost] = useState({
-        prompt : "",
-        tag : "",
-    });
-    
-    const createPrompt = (e) => {
+  const router = useRouter();
+  const { data: session } = useSession();
 
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
+  };
 
   return (
-        <Form
-            type="Create"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={createPrompt}
-        />
-    );
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-  }  
-
-export default CreatePrompt
+export default CreatePrompt;
